aeb1e07c0f37574780a5df52469ba4d095930886
Natsil <thesigilofsomnia@gmail.com>
2025-09-21 17:23:22 +0530

    options added

--- quark.py
+++ quark.py
@@ -10,14 +10,21 @@ class Task():
 
 print("--- Quark 3000: Task Tracker Supreme ---")
 print()
-print("1: Add, update, or delete a task")
+print("1: Add, edit, or delete a task")
 print("2: Update task status")
 print("3: List your tasks")
 
 command = input("What would you like to do? ")
 
 if command == "1":
-    print("ok")
+    print("1: Add a new task")
+    print("2: Edit a task")
+    print("3: Delete a task")
+    command_2 = input("Pick an option: ")
+    if command_2 == "1":
+        task_description = input("Enter a task to add to the list: ")
+        task = Task(1, task_description, "ongoing", "test", "test")
+
 elif command == "2":
     print("ok")
 elif command == "3":
