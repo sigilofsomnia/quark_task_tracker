3ba4aaeb2cdabc300dd4f1a30463c9c5c2f87500
Natsil <thesigilofsomnia@gmail.com>
2025-09-21 16:39:52 +0530

    second commit, with love

--- quark.py
+++ quark.py
@@ -7,3 +7,20 @@ class Task():
         self.status = status
         self.createdAt = createdAt
         self.updatedAt = updatedAt
+
+print("--- Quark 3000: Task Tracker Supreme ---")
+print()
+print("1: Add, update, or delete a task")
+print("2: Update task status")
+print("3: List your tasks")
+
+command = input("What would you like to do? ")
+
+if command == "1":
+    print("ok")
+elif command == "2":
+    print("ok")
+elif command == "3":
+    print("ok")
+else:
+    print("Incorrect input, please try again!")
