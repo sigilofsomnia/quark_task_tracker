bcd97b1daf292af5973c48110cccd15cdee62f1c
Natsil <thesigilofsomnia@gmail.com>
2025-09-21 16:15:20 +0530

    soup.. yummy..

--- quark.py
+++ quark.py
@@ -1,11 +1,5 @@
 # CLI task tracking program created by Natsil
 
-class Car():
-  def __init__(self, brand, model, color):
-    self.brand = brand
-    self.model = model
-    self.color = color
-
 class Task():
     def __init__(self, id, description, status, createdAt, updatedAt):
         self.id = id
