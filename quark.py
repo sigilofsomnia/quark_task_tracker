cc3cbcb6ba11975f5e2d2fd0be9f3806728abad0
Natsil <thesigilofsomnia@gmail.com>
2025-09-21 18:10:36 +0530

    stuff happened

--- quark.py
+++ quark.py
@@ -1,8 +1,12 @@
 # CLI task tracking program created by Natsil
 
+
 class Task():
-    def __init__(self, id, description, status, createdAt, updatedAt):
-        self.id = id
+    id_counter = 0
+
+    def __init__(self, description, status, createdAt, updatedAt):
+        Task.id_counter += 1
+        self.id = Task.id_counter
         self.description = description
         self.status = status
         self.createdAt = createdAt
@@ -10,29 +14,33 @@ class Task():
 
 task_list = []
 
-print("--- Quark 3000: Task Tracker Supreme ---")
-print()
-print("1: Add, edit, or delete a task")
-print("2: Update task status")
-print("3: List your tasks")
+while True:
+    print("--- Quark 3000: Task Tracker Supreme ---")
+    print()
+    print("1: Add, edit, or delete a task")
+    print("2: Update task status")
+    print("3: List your tasks")
 
-command = input("What would you like to do? ")
+    command = input("What would you like to do? ")
 
-if command == "1":
-    print("1: Add a new task")
-    print("2: Edit a task")
-    print("3: Delete a task")
-    command_2 = input("Pick an option: ")
-    if command_2 == "1":
-        task_description = input("Enter a task to add to the list: ")
-        task = Task(1, task_description, "ongoing", "test", "test")
-        task_list.append(task)
-        for task in task_list:
-            print(task.description)
+    if command == "1":
+        print("1: Add a new task")
+        print("2: Edit a task")
+        print("3: Delete a task")
+        command_2 = input("Pick an option: ")
+        if command_2 == "1":
+            task_description = input("Enter a task to add to the list: ")
+            task = Task(task_description, "ongoing", "test", "test")
+            task_list.append(task)
 
-elif command == "2":
-    print("ok")
-elif command == "3":
-    print("ok")
-else:
-    print("Incorrect input, please try again!")
+    elif command == "2":
+        print("ok")
+    elif command == "3":
+        if not task_list:
+            print("No tasks yet!")
+        else:
+            for task in task_list:
+                    print(task.id)
+                    print(task.description)
+    else:
+        print("Incorrect input, please try again!")
